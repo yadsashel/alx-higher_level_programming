80e730b20925823e72c81980520466e1772f47b7
yadsashel <ysalhi600@gmail.com>
2023-11-29 08:03:02 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
+for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
-        print(chr(letter), end="")
+        print("{}".format(chr(letter)), end="")
 
