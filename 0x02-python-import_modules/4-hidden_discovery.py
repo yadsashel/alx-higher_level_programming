9565ef6609c35931a720938593f0e7250452749f
yadsashel <ysalhi600@gmail.com>
2023-11-30 14:49:51 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import hidden_4
-    d = dir(hidden_4)
-    for i in range(0, len(d)):
-        if d[i][0:2] != "__":
-            print("{}".format(d[i]))
-
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][0] != "_" and name[i][1] != "_":
+            print(name[i])
