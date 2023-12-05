8baa21769e29f4c2e25fdd93736d708881e7befe
yadsashel <ysalhi600@gmail.com>
2023-12-04 23:49:11 -0800

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < my_list:
+        return (None)
+    if idx > my_list:
+        return (None)
+    else:
+        return(my_list[idx])
