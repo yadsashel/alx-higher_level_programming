82e84dd7d902979c0759e15361b6efbacebda934
yadsashel <ysalhi600@gmail.com>
2023-12-07 02:05:50 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,5 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-   
-    new_matrix = []
 
-    for row in matrix:
-
-        squared_row = []
-
-        for element in row:
-
-            squared_row.append(elemet ** 2)
-
-         new_matrix.append(squared_row)
-
-
-        return new_matrix
+    return [[y **2 for y in x] for x in matrix]
