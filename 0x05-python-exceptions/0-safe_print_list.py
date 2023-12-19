60a65d11c40bfac80ae423f509b52ad7e38bf29a
yadsashel <ysalhi600@gmail.com>
2023-12-19 03:57:44 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        my_list = [1, 2, 3, 4, 5]
+        print('my_list')
+        return my_list
+    except my_list
+        return  my_list
