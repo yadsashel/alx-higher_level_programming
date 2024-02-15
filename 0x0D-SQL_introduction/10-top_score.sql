7c8f4f65b065d124a5c1cff93693f63d45a5c660
yadsashel <ysalhi600@gmail.com>
2024-02-15 07:42:17 -0800

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script to list all records of second_table ordered by score (top first)
+
+-- List all records ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
+
