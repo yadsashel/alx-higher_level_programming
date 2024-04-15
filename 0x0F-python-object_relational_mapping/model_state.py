ac354b36c5379e399621f09e05ef6a5880f0cb07
yadsashel <ysalhi600@gmail.com>
2024-04-15 06:22:48 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""Modules that Define the State class representing a state MySQL database"""
+"""Defines the State class"""
 
-import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-
 class State(Base):
-    """State class with id and name attributes"""
+    """Represents a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    cities = relationship("City", backref="state", cascade="all, delete")
