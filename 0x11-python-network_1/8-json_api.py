a786186975fb1a177bee545ec454ac70002fbb91
yadsashel <ysalhi600@gmail.com>
2024-07-07 10:53:37 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-A Python script that sends a POST request to 
-http://0.0.0.0:5000/search_user
+A Python script that sends a POST request to http://0.0.0.0:5000/search_user
 with a letter as a parameter using requests package
 """
 import requests
@@ -15,7 +14,10 @@ if __name__ == "__main__":
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print(
+                "[{}] {}".format(
+                    json_response.get('id'),
+                    json_response.get('name')))
         else:
             print("No result")
     except ValueError:
