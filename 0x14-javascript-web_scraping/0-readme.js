47e29ca640df041eba0d8945a48d2bb310cb628e
yadsashel <ysalhi600@gmail.com>
2024-07-30 07:57:36 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 
-// a script that reads and prints the content of a file.
-
 const fs = require('fs');
+const args = process.argv;
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+fs.readFile(args[2], 'utf-8', (err, data) => {
+  if (err) { console.log(err); } else {
+    console.log(data);
   }
-  console.log(data);
 });
